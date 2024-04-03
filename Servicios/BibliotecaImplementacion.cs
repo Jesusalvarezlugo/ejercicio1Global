a78781bcf8841b118bd559ae37cbca0b1d577509
Jesús <csi22-jalvugo@colegioaltair.net>
2024-04-03 12:46:52 +0200

    no message

--- Servicios/BibliotecaImplementacion.cs
+++ Servicios/BibliotecaImplementacion.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ejercicio1Global.Dtos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,42 @@ using System.Threading.Tasks;
 
 namespace ejercicio1Global.Servicios
 {
-    internal class BibliotecaImplementacion
+    internal class BibliotecaImplementacion : BibliotecaInterfaz
     {
+        public void crearBiblioteca(List<BibliotecaDto> listaBiblioAnt)
+        {
+            BibliotecaDto bibliotecaNueva = nuevaBiblioteca();
+            bibliotecaNueva.IdBiblioteca = autoIdB(listaBiblioAnt);
+            listaBiblioAnt.Add(bibliotecaNueva);
+        }
+
+        private BibliotecaDto nuevaBiblioteca()
+        {
+            BibliotecaDto biblioteca = new BibliotecaDto();
+
+            Console.WriteLine("Introduzca el nombre de la biblioteca: ");
+            biblioteca.NombreBiblioteca = Console.ReadLine();
+            Console.WriteLine("Introduzca la direccion de la biblioteca: ");
+            biblioteca.Direccion = Console.ReadLine();
+
+            return biblioteca;
+
+
+        }
+
+        private long autoIdB(List<BibliotecaDto> listaBiblioAnt)
+        {
+            int tamanioLista = listaBiblioAnt.Count;
+            long nuevoId;
+            if (tamanioLista > 0)
+            {
+                nuevoId = listaBiblioAnt[tamanioLista - 1].IdBiblioteca + 1;
+            }
+            else
+            {
+                nuevoId = 1;
+            }
+            return nuevoId;
+        }
     }
 }
